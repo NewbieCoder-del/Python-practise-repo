124db3a1de4c8b049bee5f8ad72ce8178254156d
NewbieCoder-del <study4unis@gmail.com>
2025-08-16 09:30:30 +0530

    Update 06_ch10.py

--- Practise/Chapter 10/06_ch10.py
+++ Practise/Chapter 10/06_ch10.py
@@ -5,18 +5,19 @@ class Train:
     def __init__(slf, trainNo):
         slf.trainNo = trainNo
 
-    def book(harry, fro, to):
-        print(f"Ticket is booked in train no: {harry.trainNo} from {fro} to {to}") 
+    def book(hari, train_fro, train_to):
+        print(f"Ticket is booked in train no: {hari.trainNo} from {train_fro} to {train_to}") 
 
     def getStatus(self):
         print(f"Train no: {self.trainNo} is running on time") 
 
-    def getFare(self, fro, to):
-        print(f"Ticket fare in train no: {self.trainNo} from {fro} to {to} is {randint(222, 5555)}")  
+    def getFare(self, train_fro, train_to):
+        print(f"Ticket fare in train no: {self.trainNo} from {train_fro} to {train_to} is {randint(222, 5555)}")  
 
 
 t = Train(12399)
-t.book("Rampur", "Delhi")
+t.book("Raipur", "Delhi")
 t.getStatus()
-t.getFare("Rampur", "Delhi")
+t.getFare("Raipur", "Delhi")
+
 
